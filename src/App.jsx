40fa2142f1c18bf95ebf6638152503a26fdfbb7d
Yao Wang <ywang011@odu.edu>
2023-02-05 17:39:58 -0500

    update changes

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import logo from './assets/images/logo.png';
 
 function App() {
   return (
-    <base>
+    <>
       <header>
         <div id="logo-img">
           <img src={logo} />
@@ -11,7 +11,7 @@ function App() {
         <h1>Learn & Master GitHub Actions</h1>
       </header>
       <MainContent />
-    </base>
+    </>
   );
 }
 
